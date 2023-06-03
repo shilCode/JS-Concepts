1df675a35c6b451f580863ada14b0345339232a5
ShilCode <2850542@gmail.com>
2023-06-03 15:27:06 +0200

    functions

---
+++ function.js
@@ -0,0 +1,13 @@
+//dumb down function
+
+function add(a,b){
+     return a+b;
+}
+
+console.log(add(20,20))
+
+//functions that are mostly used by tester in different frameworks
+
+coolFunction = (a,b)=> a+b
+
+console.log(coolFunction(20,20));
